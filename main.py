6648b95bae069f880cb7da5170c9a32984f4f21e
Jasweer09 <jasweertadikonda@gmail.com>
2025-04-30 20:56:10 -0700

    Data Transformation is completed for model training

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from textSummarizer.logging import logger
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.pipeline.stage_02_data_validation import DataValidationPipeline
+from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationPipeline
 
 STAGE_NAME = "Data Ingestion"
 try:
     logger.info(f">>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<<<<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
-    logger.info(">>>>>>>>>> stage {STAGE_NAME} is Completed <<<<<<<<<\n\nx===========x")
+    logger.info(f">>>>>>>>>> stage {STAGE_NAME} is Completed <<<<<<<<<\n\nx===========x")
 except Exception as e:
     logger.exception(e)
     raise e
@@ -19,7 +20,18 @@ try:
     logger.info(f">>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<<<<<<<")
     data_validation = DataValidationPipeline()
     data_validation.main()
-    logger.info(">>>>>>>>>> stage {STAGE_NAME} is Completed <<<<<<<<<\n\nx===========x")
+    logger.info(f">>>>>>>>>> stage {STAGE_NAME} is Completed <<<<<<<<<\n\nx===========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Transformation"
+
+try:
+    logger.info(f">>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<<<<<<<")
+    data_transformation = DataTransformationPipeline()
+    data_transformation.main()
+    logger.info(f">>>>>>>>>> stage {STAGE_NAME} is Completed <<<<<<<<<\n\nx===========x")
 except Exception as e:
     logger.exception(e)
     raise e
--- src/textSummarizer/pipeline/stage_02_data_validation.py
+++ src/textSummarizer/pipeline/stage_02_data_validation.py
@@ -6,11 +6,9 @@ class DataValidationPipeline:
     def __init__(self):
         pass
     def main(self):
-        try:
-            config = ConfigurationManager()
-            data_validation_config = config.get_data_validation_configuration()
-            data_validation = DataValidation(config = data_validation_config)
-            data_validation.validate_all_files_exist()
-        except Exception as e:
-            raise e
+        config = ConfigurationManager()
+        data_validation_config = config.get_data_validation_configuration()
+        data_validation = DataValidation(config = data_validation_config)
+        data_validation.validate_all_files_exist()
+    
 
